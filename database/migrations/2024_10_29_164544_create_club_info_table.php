5fcb60fe20cceae0a4502969a3f46cd3c010ed63
Kyyn <maxtheo155@icloud.com>
2024-10-29 17:47:55 +0100

    Add .env.example and init tables / migrations

---
+++ database/migrations/2024_10_29_164544_create_club_info_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('club_info', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->text('name');
+            $table->text('description');
+            $table->text('location');
+            $table->timestamp('created_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('club_info');
+    }
+};
